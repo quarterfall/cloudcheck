7d0185379f30d411935900533e76f1e61b969eff
Arjan Egges <arjan@quarterfall.com>
2019-11-12 11:36:07 +0100

    Fixed missing newlines.

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-echo "$answer"
-
-echo $answer
-
 # write the answer to a source file
-echo $answer > ./src/answer.hpp
+echo "$answer" > ./src/answer.hpp
 
 # go to the src folder, build and test
 cd src
