11d326ee0eaa2285e9e34c93ee9ae59d2ef9d036
Arjan Egges <arjan@quarterfall.com>
2019-11-12 11:17:59 +0100

    Updated run script

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,11 +1,5 @@
 # write the answer to a source file
 echo $answer > ./src/answer.hpp
 
-# go to the src folder
-cd src
-
-# build the executable
-make
-
-# run the test
-./test
+# go to the src folder, build and test
+cd src && make && ./test
